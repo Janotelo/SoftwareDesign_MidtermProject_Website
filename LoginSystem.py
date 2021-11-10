ee58dd080154acbc7a9ecd77bb2d2f6e051e7712
Janotelo <jagj102999@gmail.com>
2021-11-10 12:25:03 +0000

    Working Loggin and Register Services

--- LoginSystem.py
+++ LoginSystem.py
@@ -5,60 +5,54 @@ import uuid
 from flask import Flask, request, render_template, redirect
 import os
 
-#currentlocation = os.path.dirname(os.path.abspath(__file__))
 app = Flask(__name__)
-db_name = 'User.db'
+db_name = 'UserCred.db'
 
 @app.route("/")
 def homepage():
     return render_template("homepage.html")
 
-@app.route("/", methods = ["POST"])
-def checklogin():
-    UN = request.form['Username']
-    PW = request.form['Password']
+def verify_cred(username, password):
+    conn = sqlite3.connect('test.db')
+    c = conn.cursor()
+    query = "SELECT PASSWORD FROM USER_CRED WHERE USERNAME = '{0}'".format(username)
+    c.execute(query)
+    records = c.fetchone()
+    conn.close()
+    if not records:
+        return False
+    return records[0] == password
 
-    sqlconnection = sqlite3.connection(currentlocation + "\Login.db")
-    cursor = sqlconnection.cursor()
-    query1 = "SELECT Username, Password From Users WHERE Usernmae = '{un}' AND Password = '{pw}'".format(un = UN, pw = PW)
-
-    rows = cursor.execute(query1)
-    rows = rows.fetchall()
-    if len(rows) == 1:
-        return render_template("LoggedIn.html")
-    else:
-        return render_template("/register")
-
+@app.route("/", methods = ["GET","POST"])
 def checkloginv2():
-    UN = request.form['Username']
-    PW = request.form['Password']
-
-    sqlconnection = sqlite3.connection(currentlocation + "\Login.db")
-    cursor = sqlconnection.cursor()
-    query1 = "SELECT Username, Password From Users WHERE Usernmae = '{un}' AND Password = '{pw}'".format(un = UN, pw = PW)
-
-    rows = cursor.execute(query1)
-    rows = rows.fetchall()
-    if len(rows) == 1:
-        return render_template("LoggedIn.html")
+    reqUserLog = request.form['Username']
+    reqPassLog = request.form['Password']
+    error = None
+    if request.method == 'POST':
+        if verify_cred(reqUserLog, reqPassLog):
+            error = 'login success'
+        else:
+            error = 'Invalid username/password'
     else:
-        return render_template("/register")    
+        error = 'Invalid Method'
+    return error
 
 @app.route("/register", methods = ["GET", "POST"])
-def registerpagev2():
-    reqUser = request.form['username']
-    reqPass = request.form['password']
-    conn = sqlite3.connect(db_name)
-    c = conn.cursor()
-    c.execute('''CREATE TABLE IF NOT EXISTS USER_CRED(USERNAME TEXT PRIMARY KEY NOT NULL, PASSWORD TEXT NOT NULL);''')
-    conn.commit()
-    try:
-        c.execute("INSERT INTO USER_CRED (USERNAME,PASSWORD)" "VALUES('{0}', '{1}')".format(request.form['username'],request.form['password]))
+def registerpage():
+    if request.method == "POST":
+        reqUser = request.form['Username']
+        reqPass = request.form['Password']
+        conn = sqlite3.connect(db_name)
+        c = conn.cursor()
+        c.execute('''CREATE TABLE IF NOT EXISTS USER_CRED(USERNAME TEXT PRIMARY KEY NOT NULL, PASSWORD TEXT NOT NULL);''')
         conn.commit()
-    except sqlite3.IntegrityError:
-        return "Username has been registered."
-    print('username: ', request.form['username'], 'password: ', request.form('password'))
-    return "signup success"
+        try:
+            c.execute("INSERT INTO USER_CRED (USERNAME,PASSWORD)" "VALUES('{0}', '{1}')".format(reqUser,reqPass))
+            conn.commit()
+        except sqlite3.IntegrityError:
+            return "Username has been registered."
+        print('username: ', reqUser, 'password: ', reqPass)
+    return render_template("Register.html")
 
 if __name__== "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
