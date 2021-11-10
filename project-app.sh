167b539c9e8990d381dc4f3aefbebec4d02f7515
Janotelo <jagj102999@gmail.com>
2021-11-10 15:49:10 +0000

    Updated Bash file

--- project-app.sh
+++ project-app.sh
@@ -2,13 +2,16 @@
 
 mkdir tempdir
 mkdir tempdir/templates
+mkdir tempdir/static
 
 cp LoginSystem.py tempdir/.
 cp -r templates/* tempdir/templates/.
+cp -r static/* tempdir/static/.
 
 echo "FROM python" > tempdir/Dockerfile
 echo "RUN pip install flask" >> tempdir/Dockerfile
 echo "COPY  ./templates /home/myapp/templates/" >> tempdir/Dockerfile
+echo "COPY  ./static /home/myapp/static/" >> tempdir/Dockerfile
 echo "COPY  LoginSystem.py /home/myapp/" >> tempdir/Dockerfile
 echo "EXPOSE 5050" >> tempdir/Dockerfile
 echo "CMD python3 /home/myapp/LoginSystem.py" >> tempdir/Dockerfile
