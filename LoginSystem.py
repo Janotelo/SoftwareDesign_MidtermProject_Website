d41abf5c2e9f6357bc9837bc4862a8107c008581
Janotelo <jagj102999@gmail.com>
2021-11-10 11:36:30 +0000

    Updateing Register Services

--- LoginSystem.py
+++ LoginSystem.py
@@ -6,8 +6,8 @@ from flask import Flask, request, render_template, redirect
 import os
 
 #currentlocation = os.path.dirname(os.path.abspath(__file__))
-
 app = Flask(__name__)
+db_name = 'User.db'
 
 @app.route("/")
 def homepage():
@@ -29,19 +29,36 @@ def checklogin():
     else:
         return render_template("/register")
 
+def checkloginv2():
+    UN = request.form['Username']
+    PW = request.form['Password']
+
+    sqlconnection = sqlite3.connection(currentlocation + "\Login.db")
+    cursor = sqlconnection.cursor()
+    query1 = "SELECT Username, Password From Users WHERE Usernmae = '{un}' AND Password = '{pw}'".format(un = UN, pw = PW)
+
+    rows = cursor.execute(query1)
+    rows = rows.fetchall()
+    if len(rows) == 1:
+        return render_template("LoggedIn.html")
+    else:
+        return render_template("/register")    
+
 @app.route("/register", methods = ["GET", "POST"])
-def registerpage():
-    if request.method == "POST":
-        dUN = request.form["DUsername"]
-        dPW = request.form['DPassword']
-        Uemail = request.form['Emailuser']
-        sqlconnection = sqlite3.connection(currentlocation + "\Login.db")
-        cursor = sqlconnection.cursor()
-        query1 = "INSERT INTO Users VALUES('{u}','{p}','{e}')".format(u = dUN, p = dPW, e = Uemail)
-        cursor.execute(query1)
-        sqlconnection.commit()
-        return redirect("/")
-    return render_template("Register.html")
+def registerpagev2():
+    reqUser = request.form['username']
+    reqPass = request.form['password']
+    conn = sqlite3.connect(db_name)
+    c = conn.cursor()
+    c.execute('''CREATE TABLE IF NOT EXISTS USER_CRED(USERNAME TEXT PRIMARY KEY NOT NULL, PASSWORD TEXT NOT NULL);''')
+    conn.commit()
+    try:
+        c.execute("INSERT INTO USER_CRED (USERNAME,PASSWORD)" "VALUES('{0}', '{1}')".format(request.form['username'],request.form['password]))
+        conn.commit()
+    except sqlite3.IntegrityError:
+        return "Username has been registered."
+    print('username: ', request.form['username'], 'password: ', request.form('password'))
+    return "signup success"
 
 if __name__== "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
