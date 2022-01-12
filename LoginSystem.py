44f81caf3a18582c957381d2f937b31312f17363
Janotelo <jagj102999@gmail.com>
2022-01-12 14:20:31 +0000

    Major Update | Applied Sessions and Token

--- LoginSystem.py
+++ LoginSystem.py
@@ -10,54 +10,70 @@ url = "http://127.0.0.1:5001"
 def LoginPage():
     if "token" in session:
         token = session["token"]
-        return redirect("/dashboard")
-    else:
-        return render_template("login.html")
-
-@app.route("/", methods = ["GET","POST"])
-def user_login():
-    if request.method =="POST":
-        reqEmailLog = request.form['email']
-        reqPassLog = request.form['password']
-        user_CRED = {
-                'email':reqEmailLog,
-                'password':reqPassLog,
-                }
-        json_data = requests.post(url + '/api/login', json = user_CRED, verify=False)
-        print(json_data.content)
-        des_cont = deserialize(json_data.content)
-        session["token"]= (des_cont["token"])
         return redirect("/homepage")
+    else:
+        return redirect("/login")
+
+@app.route("/login", methods = ["GET","POST"])
+def user_login():
+    if "token" in session:
+        token = session["token"]
+        return redirect("/homepage")
+    else:
+        if request.method =="POST":
+            reqEmailLog = request.form['email']
+            reqPassLog = request.form['password']
+            user_CRED = {
+                    'email':reqEmailLog,
+                    'password':reqPassLog,
+                    }
+            json_data = requests.post(url + '/api/login', json = user_CRED, verify=False)
+            print(json_data.content)
+            des_cont = deserialize(json_data.content)
+            session["token"]= (des_cont["token"])
+            return redirect("/homepage")
+        return render_template("login.html")
 
 @app.route("/register", methods = ["GET", "POST"])
 def user_register():
-    if request.method == "POST":
-        regEmail = request.form['email']
-        regPass = request.form['password']
-        user_CRED = {
-            'email':regEmail,
-            'password':regPass
-            }
-        requests.post(url + '/api/register', json = user_CRED, verify=False)
-        return redirect("/")
-    return render_template("register.html")
+    if "token" in session:
+        token = session["token"]
+        return redirect("/homepage")
+    else:
+        if request.method == "POST":
+            regEmail = request.form['email']
+            regPass = request.form['password']
+            regFname = request.form['fname']
+            regLname = request.form['lname']
+            regMobile_Number = request.form['mobile_number']
+
+            user_CRED = {
+                'email':regEmail,
+                'password':regPass,
+                "fname" : regFname,
+                "lname" : regLname,
+                "mobile_number" : regMobile_Number
+                }
+            requests.post(url + '/api/register', json = user_CRED, verify=False)
+            return redirect("/")
+        return render_template("register.html")
 
 @app.route("/homepage", methods = ["GET", "POST"])
-def dashboard():
+def homepage():
     if "token" in session:
         return render_template("homepage.html")
     else:
         return redirect("/")
         
 @app.route("/monitoring", methods = ["GET", "POST"])
-def dashboard():
+def monitoring():
     if "token" in session:
         return render_template("monitoring.html")
     else:
         return redirect("/")
 
 @app.route("/team", methods = ["GET", "POST"])
-def dashboard():
+def team():
     if "token" in session:
         return render_template("team.html")
     else:
